e797c59d7e4382fb791209c8e243972206aea514
CaioRenatoDot <caiorenato146@gmail.com>
2026-05-20 21:25:45 -0300

    feat: começo de projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="grid min-h-screen place-items-center bg-slate-50">
+      <h1 className="text-4xl font-bold text-slate-900">Hello World</h1>
+    </main>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
